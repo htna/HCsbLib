2fedf6c2678455bcddfe5ef435aa6dda87457515
Hyuntae Na <htna@users.noreply.github.com>
2018-01-29 17:54:51 -0500

    Coarse.CoarseHessForc

---
+++ HCsbLib/HCsbLib/HTLib2.Bioinfo/Coarse.CoarseHessForc/Coarse.CoarseHessForc.CoarseHessForcSimple.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HTLib2;
+using HTLib2.Bioinfo;
+
+namespace HTLib2.Bioinfo
+{
+    public partial class Coarse
+    {
+        public static partial class CoarseHessForc
+        {
+            public static partial class CoarseHessForcSimple
+            {
+            }
+        }
+    }
+}
