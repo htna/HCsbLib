baee239b1e0bd8a59bf38f9ce7c2c95a5172cbfd
Hyuntae Na <htna@users.noreply.github.com>
2019-07-08 19:33:41 -0400

    refactoring

---
+++ HCsbLib/HCsbLib/HTLib2/HStatic/HStatic.HContains.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HTLib2
+{
+    public static partial class HStatic
+    {
+        public static bool HContainsStartsWith(this IEnumerable<string> list, string startswith)
+        {
+            foreach(string str in list.HEnumByStartsWith(startswith))
+                return true;
+            return false;
+        }
+    }
+}
