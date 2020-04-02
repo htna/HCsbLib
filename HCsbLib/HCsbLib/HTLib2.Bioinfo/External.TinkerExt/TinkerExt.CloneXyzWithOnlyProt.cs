14e32118fc50b452e0819d59085744e87c351ed2
Hyuntae Na <htna@users.noreply.github.com>
2020-04-01 21:26:44 -0400

    Refactoring: * Program_htna_20180225.BuildXyzOnlyProt(xyz); -> TinkerExt.CloneXyzWithOnlyProt(this xyz);

---
+++ HCsbLib/HCsbLib/HTLib2.Bioinfo/External.TinkerExt/TinkerExt.CloneXyzWithOnlyProt.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HTLib2.Bioinfo
+{
+    public static partial class TinkerExt
+    {
+        public static Tinker.Xyz CloneXyzWithOnlyProt(this Tinker.Xyz xyz)
+        {
+            List<Tinker.Xyz.Atom> protein = new List<Tinker.Xyz.Atom>();
+            List<Tinker.Xyz.Atom> waters  = new List<Tinker.Xyz.Atom>();
+            foreach(var atom in xyz.atoms)
+            {
+                if(atom.AtomType == "OT " || atom.AtomType == "HT ")
+                    waters.Add(atom);
+                else
+                    protein.Add(atom);
+            }
+
+            var xyz_nowater = xyz.CloneByRemoveIds(waters.HListId());
+            var xyz_prot    = xyz_nowater.CloneByReindex(1);
+
+            return xyz_prot;
+        }
+    }
+}
