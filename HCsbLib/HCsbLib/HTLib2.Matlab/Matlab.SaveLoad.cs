564867ff12ddaf8067285a314b55447a6e6d95fe
Hyuntae Na <htna@users.noreply.github.com>
2020-05-27 23:18:41 -0400

    Refactoring: * add "string varname" as an option * update codes that sets bUseFile

--- HCsbLib/HCsbLib/HTLib2.Matlab/Matlab.SaveLoad.cs
+++ HCsbLib/HCsbLib/HTLib2.Matlab/Matlab.SaveLoad.cs
@@ -6,30 +6,38 @@ namespace HTLib2
 {
 	public partial class Matlab
 	{
-        public static void SaveMatrix(string filename, double[,] matrix, bool bUseFile=false)
+        public static void SaveMatrix(string filename, double[,] matrix, string varname=null, bool bUseFile=false)
         {
-            PutMatrix("htlib2matlab_SavMat", matrix, bUseFile);
-            Execute("save('$$','htlib2matlab_SavMat','-v4')", "$$", filename);
-            Execute("clear htlib2matlab_SavMat;");
+            if(varname == null)
+                varname = "htlib2matlab_SavMat";
+            PutMatrix(varname, matrix, bUseFile);
+            Execute("save('$$','$1','-v4')", "$$", filename, "$1", varname);
+            Execute("clear "+varname+";");
         }
-        public static double[,] LoadMatrix(string filename, bool bUseFile=false)
+        public static double[,] LoadMatrix(string filename, string varname=null, bool bUseFile=false)
         {
-            Execute("load('$$','htlib2matlab_SavMat')", "$$", filename);
-            double[,] mat = GetMatrix("htlib2matlab_SavMat", bUseFile);
-            Execute("clear htlib2matlab_SavMat;");
+            if(varname == null)
+                varname = "htlib2matlab_SavMat";
+            Execute("load('$$','$1')", "$$", filename, "$1", varname);
+            double[,] mat = GetMatrix(varname, bUseFile);
+            Execute("clear "+varname+";");
             return mat;
         }
-        public static void SaveVector(string filename, double[] vector)
+        public static void SaveVector(string filename, double[] vector, string varname=null)
         {
-            PutVector("htlib2matlab_SavMat", vector);
-            Execute("save('$$','htlib2matlab_SavMat','-v4')", "$$", filename);
-            Execute("clear htlib2matlab_SavMat;");
+            if(varname == null)
+                varname = "htlib2matlab_SavMat";
+            PutVector(varname, vector);
+            Execute("save('$$','$1','-v4')", "$$", filename, "$1", varname);
+            Execute("clear "+varname+";");
         }
-        public static double[] LoadVector(string filename)
+        public static double[] LoadVector(string filename, string varname=null)
         {
-            Execute("load('$$','htlib2matlab_SavMat')", "$$", filename);
-            double[] mat = GetVector("htlib2matlab_SavMat");
-            Execute("clear htlib2matlab_SavMat;");
+            if(varname == null)
+                varname = "htlib2matlab_SavMat";
+            Execute("load('$$','$1')", "$$", filename, "$1", varname);
+            double[] mat = GetVector(varname);
+            Execute("clear "+varname+";");
             return mat;
         }
     }
