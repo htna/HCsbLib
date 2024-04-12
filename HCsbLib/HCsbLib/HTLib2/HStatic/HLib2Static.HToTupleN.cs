f9f0339a9e163271c01b85899dac42cea3e76605
htna <htna@hotmail.com>
2024-04-12 19:04:58 -0400

    add HToValueTuple_()

--- HCsbLib/HCsbLib/HTLib2/HStatic/HLib2Static.HToTupleN.cs
+++ HCsbLib/HCsbLib/HTLib2/HStatic/HLib2Static.HToTupleN.cs
@@ -12,5 +12,9 @@ namespace HTLib2
         public static Tuple<T, T, T, T> HToTuple4<T>(this IList<T> values) { return new Tuple<T, T, T, T>(values[0], values[1], values[2], values[3]); }
 
         public static Tuple<T, U> HToTuple<T, U>(this KeyValuePair<T, U> values) { return new Tuple<T, U>(values.Key, values.Value); }
+
+        public static ValueTuple<T, T      > HToValueTuple2<T>(this IList<T> values) { return new ValueTuple<T, T      >(values[0], values[1]                      ); }
+        public static ValueTuple<T, T, T   > HToValueTuple3<T>(this IList<T> values) { return new ValueTuple<T, T, T   >(values[0], values[1], values[2]           ); }
+        public static ValueTuple<T, T, T, T> HToValueTuple4<T>(this IList<T> values) { return new ValueTuple<T, T, T, T>(values[0], values[1], values[2], values[3]); }
     }
 }
