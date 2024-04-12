c76c5e11b74f06386b253504e1833cf7f76c84a4
htna <htna@hotmail.com>
2024-04-12 03:44:37 -0400

    add debug

--- HCsbLib/HCsbLib/HTLib2/HStatic.HMath/HMath.HSort.cs
+++ HCsbLib/HCsbLib/HTLib2/HStatic.HMath/HMath.HSort.cs
@@ -14,7 +14,12 @@ namespace HTLib2
         [MethodImpl(MethodImplOptions.AggressiveInlining)] public static void SortDecr<T>(ref (T,T) ab) where T : IComparable<T> { if(ab.Item1.CompareTo(ab.Item2) < 0) Swap(ref ab.Item1, ref ab.Item2); }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static (double,double) HSort((double a, double b) tuple) { return HSort(tuple.a, tuple.b); }
+        public static (double,double) HSort((double a, double b) tuple)
+        {
+            var ab = HSort(tuple.a, tuple.b);
+            HDebug.Assert(ab.Item1 <= ab.Item2);
+            return ab;
+        }
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static (double,double) HSort(double a, double b)
         {
@@ -22,7 +27,12 @@ namespace HTLib2
             else      return (b,a);
         }
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static (double,double,double) HSort((double a, double b, double c) tuple) { return HSort(tuple.a, tuple.b, tuple.c); }
+        public static (double,double,double) HSort((double a, double b, double c) tuple)
+        {
+            var abc = HSort(tuple.a, tuple.b, tuple.c);
+            HDebug.Assert(abc.Item1 <= abc.Item2 && abc.Item2 <= abc.Item3);
+            return abc;
+        }
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static (double,double,double) HSort(double a, double b, double c)
         {
