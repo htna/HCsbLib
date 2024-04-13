4a1dde946b61869204ae516febc6bdd7a1d007d7
htna <htna@hotmail.com>
2024-04-13 02:38:15 -0400

    update HHist()

--- HCsbLib/HCsbLib/HTLib2/HStatic/HStatic.HHist.cs
+++ HCsbLib/HCsbLib/HTLib2/HStatic/HStatic.HHist.cs
@@ -7,28 +7,31 @@ namespace HTLib2
 {
     public static partial class HStatic
     {
-        public static List<((double min, double max) bin, int count)> HHist(this IEnumerable<double> values, double bin_min, double bin_max, double bin_width)
+        public static List<((T min, T max) bin, int count)> HHist<T>(this IEnumerable<T> values, T bin_min, T bin_max, T bin_width)
         {
-            List<(double min, double max)> bins      = new List<(double min, double max)>();
+            dynamic binmin = bin_min;
+            dynamic binmax = bin_max;
+            dynamic binwidth = bin_width;
+            List<(T min, T max)> bins      = new List<(T min, T max)>();
             List<int>                      bin_count = new List<int>();
             // create bins
-            for(int i=0; (bin_min + bin_width*i)<bin_max; i++)
+            for(int i=0; (binmin + binwidth*i)<binmax; i++)
             {
-                double bmin = bin_min + bin_width*i;
-                double bmax = bmin    + bin_width;
+                T bmin = binmin + binwidth*i;
+                T bmax = binmin + binwidth*(i+1);
                 bins.Add((bmin,bmax));
                 bin_count.Add(0);
             }
             HDebug.Assert(bins.Count == bin_count.Count);
             // collect counts
-            foreach(double value in values)
+            foreach(T value in values)
             {
-                int ibin = (int)((value - bin_min)/bin_width);
+                int ibin = (int)((value - binmin)/binwidth);
                 if((ibin < 0) || (bin_count.Count <= ibin)) continue;
                 bin_count[ibin] ++;
             }
             // return format
-            List<((double min, double max) bin, int count)> hist = new List<((double min, double max) bin, int count)>();
+            List<((T min, T max) bin, int count)> hist = new List<((T min, T max) bin, int count)>();
             for(int i=0; i<bins.Count; i++)
                 hist.Add((bins[i], bin_count[i]));
 
