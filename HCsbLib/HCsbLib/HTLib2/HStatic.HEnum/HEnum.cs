1574d88f76a637336798608a91dc5342c00c2cc5
htna <htna@hotmail.com>
2024-04-19 15:23:12 -0400

    HEnumEnumValues(...)

--- HCsbLib/HCsbLib/HTLib2/HStatic.HEnum/HEnum.cs
+++ HCsbLib/HCsbLib/HTLib2/HStatic.HEnum/HEnum.cs
@@ -7,5 +7,29 @@ namespace HTLib2
 {
     public static partial class HEnum
     {
+        public static IEnumerable<TElem> HEnumEnumValues<TKey,TElem>(this Dictionary<TKey,List<TElem>> dict)
+        {
+            foreach(var list in dict.Values)
+                foreach(var item in list)
+                    yield return item;
+        }
+        public static IEnumerable<TElem> HEnumEnumValues<TKey,TElem>(this Dictionary<TKey,TElem[]> dict)
+        {
+            foreach(var list in dict.Values)
+                foreach(var item in list)
+                    yield return item;
+        }
+        public static IEnumerable<TElem> HEnumEnumValues<TKey,TElem>(this Dictionary<TKey,IList<TElem>> dict)
+        {
+            foreach(var list in dict.Values)
+                foreach(var item in list)
+                    yield return item;
+        }
+        public static IEnumerable<TElem> HEnumEnumValues<TKey,TElem>(this Dictionary<TKey,HashSet<TElem>> dict)
+        {
+            foreach(var list in dict.Values)
+                foreach(var item in list)
+                    yield return item;
+        }
     }
 }
