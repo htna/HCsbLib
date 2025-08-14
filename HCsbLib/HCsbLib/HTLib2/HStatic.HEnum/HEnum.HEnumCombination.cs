20476f9b512d18521533e47d51981b468d1a15ee
htna <htna@hotmail.com>
2025-08-14 00:21:53 -0400

    .

--- HCsbLib/HCsbLib/HTLib2/HStatic.HEnum/HEnum.HEnumCombination.cs
+++ HCsbLib/HCsbLib/HTLib2/HStatic.HEnum/HEnum.HEnumCombination.cs
@@ -33,5 +33,11 @@ namespace HTLib2
             foreach(var comb in HEnumCombination3(values))
                 yield return comb.HToArray();
         }
+        public static IEnumerable<Tuple<T1,T2>> HEnumCombination2<T1,T2>(IList<T1> values1, IList<T2> values2)
+        {
+            foreach(T1 val1 in values1)
+                foreach(T2 val2 in values2)
+                    yield return new Tuple<T1,T2>(val1,val2);
+        }
     }
 }
