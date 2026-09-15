b31af33117c4cc968c0642cc394834b083ca9c71
htna <htna@hotmail.com>
2026-09-14 21:49:15 -0400

    update CellSearch

--- HCsbLib/HCsbLib/HTLib2/HMath.Geometry.CellSearch/Geometry.CellSearch.cs
+++ HCsbLib/HCsbLib/HTLib2/HMath.Geometry.CellSearch/Geometry.CellSearch.cs
@@ -8,60 +8,152 @@ namespace HTLib2
     public static partial class GeometryStatic
     {
     }
-	public partial class Geometry
-	{
+    public partial class Geometry
+    {
         public partial class CellSearch
         {
-            public readonly double CellSize;
-            private readonly Dictionary<(int x, int y, int z), List<(double x,double y,double z)>> cells;
+            public double cellsize;
 
-            private CellSearch(double cellSize)
-            {
-                HDebug.Exception((cellSize <= 0), "cellSize must be positive.");
+            // Absolute cell index corresponding to cells[0,0,0].
+            private (int x, int y, int z) basecell;
+            private List<(double x, double y, double z)>[,,] cells;
 
-                CellSize = cellSize;
-                cells = new Dictionary<(int,int,int), List<(double,double,double)>>();
-            }
+            //private CellSearch
+            //    ( double cellSize
+            //    , (int x, int y, int z) basecell
+            //    , int sizex
+            //    , int sizey
+            //    , int sizez
+            //    )
+            //{
+            //    HDebug.Exception(cellSize <= 0, "cellSize must be positive.");
+            //
+            //    CellSize = cellSize;
+            //    BaseCell = basecell;
+            //    cells    = new List<(double x, double y, double z)>[sizex, sizey, sizez];
+            //}
 
             public static CellSearch FromPoints(double[][] points, double cellSize)
             {
-                CellSearch search = new CellSearch(cellSize);
-                foreach (double[] point in points)
-                    search.Add(point);
-                return search;
+                HDebug.Exception(points == null, "points must not be null.");
+                HDebug.Exception(cellSize <= 0, "cellSize must be positive.");
+
+                if(points.Length == 0)
+                {
+                    return new CellSearch
+                    {
+                        cellsize = cellSize,
+                        basecell = (0,0,0),
+                        cells    = new List<(double x, double y, double z)>[0, 0, 0]
+                    };
+                }
+
+                double minx = double.PositiveInfinity;
+                double miny = double.PositiveInfinity;
+                double minz = double.PositiveInfinity;
+                double maxx = double.NegativeInfinity;
+                double maxy = double.NegativeInfinity;
+                double maxz = double.NegativeInfinity;
+
+                foreach(double[] point in points)
+                {
+                    HDebug.Assert(point != null && point.Length == 3);
+
+                    minx = Math.Min(minx, point[0]);
+                    miny = Math.Min(miny, point[1]);
+                    minz = Math.Min(minz, point[2]);
+
+                    maxx = Math.Max(maxx, point[0]);
+                    maxy = Math.Max(maxy, point[1]);
+                    maxz = Math.Max(maxz, point[2]);
+                }
+
+                // Absolute cell containing the minimum coordinates.
+                // This cell is mapped to cells[0,0,0].
+                var basecell =
+                (
+                    x: (int)Math.Floor(minx / cellSize),
+                    y: (int)Math.Floor(miny / cellSize),
+                    z: (int)Math.Floor(minz / cellSize)
+                );
+
+                // Absolute cell containing the maximum coordinates.
+                var maxcell =
+                (
+                    x: (int)Math.Floor(maxx / cellSize),
+                    y: (int)Math.Floor(maxy / cellSize),
+                    z: (int)Math.Floor(maxz / cellSize)
+                );
+
+                int sizex = maxcell.x - basecell.x + 1;
+                int sizey = maxcell.y - basecell.y + 1;
+                int sizez = maxcell.z - basecell.z + 1;
+
+                CellSearch cellsearch =
+                    new CellSearch
+                    {
+                        cellsize = cellSize,
+                        basecell = basecell,
+                        cells    = new List<(double x, double y, double z)>[sizex, sizey, sizez]
+                    };
+
+                foreach(double[] point in points)
+                    cellsearch.Add(point);
+
+                return cellsearch;
             }
 
             private void Add(double[] point)
             {
-                HDebug.Assert(point != null && point.Length == 3); // "point must be a double array of length 3.");
+                HDebug.Assert(point != null && point.Length == 3);
 
                 var cell = GetCellIndex(point);
-                if (cells.TryGetValue(cell, out List<(double,double,double)> list) == false)
+
+                HDebug.Assert
+                (
+                    0 <= cell.x && cell.x < cells.GetLength(0) &&
+                    0 <= cell.y && cell.y < cells.GetLength(1) &&
+                    0 <= cell.z && cell.z < cells.GetLength(2)
+                );
+
+                List<(double x, double y, double z)> cellpoints = cells[cell.x, cell.y, cell.z];
+
+                if(cellpoints == null)
                 {
-                    list = new List<(double,double,double)>();
-                    cells.Add(cell, list);
+                    cellpoints = new List<(double x, double y, double z)>();
+                    cells[cell.x, cell.y, cell.z] = cellpoints;
                 }
-                list.Add((point[0], point[1], point[2]));
+
+                cellpoints.Add( (point[0], point[1], point[2]) );
             }
 
+            // ============================================================
+            // Convert Cartesian coordinate into local 3D-array index.
+            // ============================================================
             private (int x, int y, int z) GetCellIndex(double[] point)
             {
-                // Get voxel index
-                int x = (int)Math.Floor(point[0] / CellSize);
-                int y = (int)Math.Floor(point[1] / CellSize);
-                int z = (int)Math.Floor(point[2] / CellSize);
-
-                return (x, y, z);
+                HDebug.Assert(point != null && point.Length == 3);
+                int x = (int)Math.Floor(point[0] / cellsize) - basecell.x;
+                int y = (int)Math.Floor(point[1] / cellsize) - basecell.y;
+                int z = (int)Math.Floor(point[2] / cellsize) - basecell.z;
+                return (x,y,z);
             }
 
             // ============================================================
-            // Get cells that can contain points within cutoff
+            // Relative cells that can possibly contain a point
+            // within cutoff.
+            //
+            // The search point can be anywhere in cell (0,0,0).
+            // Therefore the minimum cell-to-cell distance is used.
             // ============================================================
-            private (int dx, int dy, int dz)[] GetSearchCellIndices(double cutoff)
+            private IEnumerable<(int dx, int dy, int dz)> GetSearchCellIndices(double cutoff)
             {
                 HDebug.Assert(cutoff > 0);
 
-                int    range   = (int)Math.Ceiling(cutoff / CellSize);
+                // For a relative cell dx:
+                // minimum separation = max(0, |dx|-1) * CellSize
+                // Therefore the largest necessary |dx| is floor(cutoff / CellSize) + 1.
+                int    range   = (int)Math.Floor(cutoff / cellsize) + 1;
                 double cutoff2 = cutoff * cutoff;
 
                 List<(int dx, int dy, int dz)> searchcells = new List<(int dx, int dy, int dz)>();
@@ -70,46 +162,55 @@ namespace HTLib2
                 for(int dy=-range; dy<=range; dy++)
                 for(int dz=-range; dz<=range; dz++)
                 {
-                    // minimum possible distance between cell (0,0,0)
-                    // and cell (dx,dy,dz)
-
-                    double vx = Math.Max(0, Math.Abs(dx)-1) * CellSize;
-                    double vy = Math.Max(0, Math.Abs(dy)-1) * CellSize;
-                    double vz = Math.Max(0, Math.Abs(dz)-1) * CellSize;
+                    double vx = Math.Max(0, Math.Abs(dx)-1) * cellsize;
+                    double vy = Math.Max(0, Math.Abs(dy)-1) * cellsize;
+                    double vz = Math.Max(0, Math.Abs(dz)-1) * cellsize;
 
                     double dist2 = vx*vx + vy*vy + vz*vz;
 
                     if(dist2 <= cutoff2)
-                        searchcells.Add((dx,dy,dz));
+                        yield return (dx,dy,dz);
                 }
-
-                return searchcells.ToArray();
             }
+
+
             // ============================================================
-            // Enumerate all points within cutoff
+            // Enumerate stored points within cutoff.
             // ============================================================
+
             public IEnumerable<(double x, double y, double z)> Search
-                (double[] point, double cutoff)
+                ( double[] point
+                , double cutoff
+                , Dictionary<double, (int dx, int dy, int dz)[]> buff_cutoff_searchcells = null
+                )
             {
                 HDebug.Assert(point != null && point.Length == 3);
                 HDebug.Assert(cutoff > 0);
 
                 var icell = GetCellIndex(point);
 
-                var searchcells = GetSearchCellIndices(cutoff);
+                (int dx, int dy, int dz)[] searchcells;
+                if(buff_cutoff_searchcells != null && buff_cutoff_searchcells.ContainsKey(cutoff))
+                    searchcells = buff_cutoff_searchcells[cutoff];
+                else
+                    searchcells = GetSearchCellIndices(cutoff).ToArray();
 
                 double cutoff2 = cutoff * cutoff;
 
                 foreach(var dcell in searchcells)
                 {
-                    var cell =
-                    (
-                        icell.x + dcell.dx,
-                        icell.y + dcell.dy,
-                        icell.z + dcell.dz
-                    );
+                    int cellx = icell.x + dcell.dx;
+                    int celly = icell.y + dcell.dy;
+                    int cellz = icell.z + dcell.dz;
 
-                    if(cells.TryGetValue(cell, out var cellpoints) == false)
+                    // The search point can be outside the bounding box.
+                    if(cellx < 0 || cells.GetLength(0) <= cellx) continue;
+                    if(celly < 0 || cells.GetLength(1) <= celly) continue;
+                    if(cellz < 0 || cells.GetLength(2) <= cellz) continue;
+
+                    List<(double x, double y, double z)> cellpoints = cells[cellx, celly, cellz];
+
+                    if(cellpoints == null)
                         continue;
 
                     foreach(var neighbor in cellpoints)
@@ -126,5 +227,5 @@ namespace HTLib2
                 }
             }
         }
-	}
+    }
 }
